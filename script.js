e1eaebeeb2e3c236cf1881acfca1da455e7319d2
yang2674 <steve@softwareforgood.com>
2022-04-10 17:04:59 -0500

    centered buttons, img, and title for iphone5

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 const punches = ["images/punch-low.gif", "images/punch-mid.gif", "images/punch-high.gif"];
 const numPunches = punches.length;
 
-//HERE: not cycling through punches array
-var punchButton = document.querySelector('#punch').addEventListener('click', function () {
-    for (i = 0; i < numPunches; i++) {
-        document.querySelector('img').setAttribute('src', punches[i]);
-    }
-});
+//onlick next image in punches array
+function nextPunch() {
+    document.querySelector('#punch').addEventListener('click', function () {
+        for (i = 0; i < numPunches; i++) {
+            document.querySelector('img').setAttribute('src', punches[i]);
+        }
+    });
+}
